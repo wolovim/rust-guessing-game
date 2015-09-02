c1d167b272546466d89b89d3a42949a192f3723e
Marc Garreau <marcdgarreau@gmail.com>
2015-09-02 08:11:17 -0600

    Loops for actual gameplay

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,38 @@
 extern crate rand;
 
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
-    println!("What number am I thinking of?");
+    println!("I'm thinking of a number...");
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
     println!("Psst, it's: {}", secret_number);
 
-    let mut guess = String::new();
+    loop {
+        println!("What's your guess?");
 
-    io::stdin().read_line(&mut guess)
-        .ok()
-        .expect("Failed to read line");
+        let mut guess = String::new();
 
-    println!("You guessed: {}", guess);
+        io::stdin().read_line(&mut guess)
+            .ok()
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_)  => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less    => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal   => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
